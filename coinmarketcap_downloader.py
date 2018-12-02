a89c2a2ce0dc6eed1d8222e4ca42e08ca4c2c381
kianhean <kianhean@gmail.com>
2018-12-02 10:50:02 +0800

    more reliable html scrape notify on exception added delay due to request limiter if already scraped then skip (delete files if you want to rerun)

--- coinmarketcap_downloader.py
+++ coinmarketcap_downloader.py
@@ -1,28 +1,33 @@
+import time
+import urllib.request
+from pathlib import Path
+
 import pandas as pd
+from bs4 import BeautifulSoup
 
 
 def all_currencies():
     """Get List of all Currencies
-    
+
     Returns:
         [List] -- [List of all Currencies]
     """
-
-    url = 'https://coinmarketcap.com/all/views/all/'
-    df = pd.read_html(url, attrs = {'id': 'currencies-all'})[0]
-
     coins = []
-    for name in df['Name']:
-        coin_name = (name.lower().split(' ')[1:])
-        final = ''
-        for x in coin_name:
-            final += x + '-'
-        coins.append(final[:-1])
+    url = 'https://coinmarketcap.com/all/views/all/'
+
+    html_page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html_page, features="lxml")
+    all_links = [a.get('href') for a in soup.find_all('a', href=True)]
+
+    for link in list(set(all_links)):
+        if '/currencies/' in link:
+            coins.append(link.split('/')[2])
+
     return coins
 
 def download_data(currency, start_date, end_date):
     """Download Historical Data to File
-    
+
     Arguments:
         currency {[str]} -- [currency of crypto]
         start_date {[str]} -- [date yyyymmdd]
@@ -34,17 +39,19 @@ def download_data(currency, start_date, end_date):
     try:
         # Get Html to DF
         url = 'https://coinmarketcap.com/currencies/' + currency + '/historical-data/' + '?start=' \
-                                                        + start_date + '&end=' + end_date
-        df = pd.read_html(url, attrs = {'class': 'table'})[0]
-        
+            + start_date + '&end=' + end_date
+        df = pd.read_html(url, attrs={'class': 'table'})[0]
+
         # Clean data
-        df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap']
+        df.columns = ['Date', 'Open', 'High',
+                      'Low', 'Close', 'Volume', 'Market Cap']
         df['Date'] = pd.to_datetime(df['Date'], format='%b %d, %Y')
 
         # Save to CSV
         df.to_csv('data/' + currency + '.csv', index=False)
-    except:
+    except Exception as e:
         print("Error : Could not Download - " + currency)
+        print(e)
 
 
 if __name__ == "__main__":
@@ -55,4 +62,9 @@ if __name__ == "__main__":
     today = pd.to_datetime('today').strftime('%Y%m%d')
 
     for currency in currencies:
-        download_data(currency, '20000101', today)
+
+        my_file = Path('data/' + currency + '.csv')
+
+        if my_file.is_file() == False:
+            time.sleep(2)
+            download_data(currency, '20000101', today)
